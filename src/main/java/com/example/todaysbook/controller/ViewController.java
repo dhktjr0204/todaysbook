70e539ee59b40239c9a76d773499db2a42f4cd9c
fernandokkang <spineattack@gmail.com>
2024-04-26 10:55:30 +0900

    Feat: index.html, search.html 추가

---
+++ src/main/java/com/example/todaysbook/controller/ViewController.java
@@ -0,0 +1,27 @@
+package com.example.todaysbook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/index")
+    public String index(Model model) {
+
+        return "index";
+    }
+
+    @GetMapping("/search")
+    public String search(Model model) {
+
+        return "book/search";
+    }
+
+    @GetMapping("/detail")
+    public String detail(Model model) {
+
+        return "book/detail";
+    }
+}
