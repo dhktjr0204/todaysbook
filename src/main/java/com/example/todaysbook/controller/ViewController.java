b14ed7dad905aedf56ac9da1e4219d5d3cce57da
fernandokkang <spineattack@gmail.com>
2024-04-28 18:23:02 +0900

    Feat: 마이페이지 관련 html 작성

--- src/main/java/com/example/todaysbook/controller/ViewController.java
+++ src/main/java/com/example/todaysbook/controller/ViewController.java
@@ -10,6 +10,10 @@ public class ViewController {
     @GetMapping("/index")
     public String index(Model model) {
 
+        String userName = "테스트";
+
+        model.addAttribute("userName", userName);
+
         return "index";
     }
 
@@ -29,5 +33,71 @@ public class ViewController {
     public String registration(Model model) {
 
         return "user/registration";
+    }
+
+    @GetMapping("/mypage/updateinfo")
+    public String mypageUpdateInfo(Model model) {
+
+        return "user/mypage/update-info";
+    }
+
+    @GetMapping("/mypage/updatepw")
+    public String mypageUpdatePw(Model model) {
+
+        return "user/mypage/update-password";
+    }
+
+    @GetMapping("/mypage/orderlist")
+    public String orderList(Model model) {
+
+        return "user/mypage/orderlist";
+    }
+
+    @GetMapping("/mypage/review")
+    public String review(Model model) {
+
+        return "user/mypage/review";
+    }
+
+    @GetMapping("/mypage/mileage")
+    public String mileage(Model model) {
+
+        return "user/mypage/mileage";
+    }
+
+    @GetMapping("/mypage/favoritebook")
+    public String favoritebook(Model model) {
+
+        return "user/mypage/favorite-book";
+    }
+
+    @GetMapping("/mypage/my_recommend_list")
+    public String myRecommendList(Model model) {
+
+        return "user/mypage/my-recommendlist";
+    }
+
+    @GetMapping("/mypage/users_recommend_list")
+    public String userRecommendList(Model model) {
+
+        return "user/mypage/users-recommendlist";
+    }
+
+    @GetMapping("/mypage/user/delivery")
+    public String userDelivery(Model model) {
+
+        return "user/mypage/delivery";
+    }
+
+    @GetMapping("/mypage/user/order_detail")
+    public String userOrderDetail(Model model) {
+
+        return "user/mypage/orderlist-detail";
+    }
+
+    @GetMapping("/mypage/create_recommendlist")
+    public String createRecommendList(Model model) {
+
+        return "user/mypage/create-recommendlist";
     }
 }
