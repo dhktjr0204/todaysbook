5ad9b5f9f36c111f250aa6cf41583a0dfc216b89
fernandokkang <110192117+fernandokkang@users.noreply.github.com>
2024-05-07 09:34:44 +0900

    Update TodaysbookApplication.java

--- src/main/java/com/example/todaysbook/TodaysbookApplication.java
+++ src/main/java/com/example/todaysbook/TodaysbookApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@SpringBootApplication
+@SpringBootApplication
 @EnableJpaAuditing
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+//@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class TodaysbookApplication {
 
     public static void main(String[] args) {
