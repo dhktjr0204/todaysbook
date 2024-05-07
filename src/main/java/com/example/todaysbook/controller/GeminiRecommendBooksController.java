860be74b0c10b4b521a27bf6df4371efcbb76a2c
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-05-07 11:57:20 +0900

    Feat: index.html ui 수정

--- src/main/java/com/example/todaysbook/controller/GeminiRecommendBooksController.java
+++ src/main/java/com/example/todaysbook/controller/GeminiRecommendBooksController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
+// MainController에서 TodayRecommendBooks를 사용하기 때문에
+// 해당 컨트롤러는 현재 아무곳에서 사용되지 않음. 추후 사용될 수 있음. EX) AJAX 비동기
+
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/gemini")
