243ea32f9c573ccdda35a433efa847ba9dfc1946
dhktjr0204 <dhktjr0204442@gmail.com>
2024-05-07 09:11:39 +0900

    Feat: 신간 등록 기능

--- src/main/java/com/example/todaysbook/controller/AdminController.java
+++ src/main/java/com/example/todaysbook/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.example.todaysbook.controller;
 import com.example.todaysbook.domain.dto.AdminUserDto;
 import com.example.todaysbook.domain.dto.BookDto;
 import com.example.todaysbook.service.AdminService;
+import com.example.todaysbook.util.Pagination;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,52 +14,56 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
     private final AdminService adminService;
-    private final int VISIBLE_PAGE=5;
+    private final int VISIBLE_PAGE = 5;
 
     //유저 관리
     @GetMapping("/userlist")
-    public String allUserList(@PageableDefault(page = 0, size = 5, sort="id", direction = Sort.Direction.ASC) Pageable pageable,
-                           Model model){
+    public String allUserList(@PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
+                              Model model) {
         Page<AdminUserDto> allUserList = adminService.findAllUser(pageable);
 
-        HashMap<String,Integer> pages=calculatePage(allUserList.getPageable().getPageNumber(), allUserList.getTotalPages());
-        int startPage=pages.get("startPage");
-        int endPage=pages.get("endPage");
+        HashMap<String, Integer> pages = Pagination.calculatePage(allUserList.getPageable().getPageNumber(), allUserList.getTotalPages());
+        int startPage = pages.get("startPage");
+        int endPage = pages.get("endPage");
 
         model.addAttribute("dto", allUserList);
-        model.addAttribute("startPage",startPage);
+        model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
 
         return "admin/userlist";
     }
 
     @GetMapping("/userlist/search")
-    public String searchUserList(@PageableDefault(page = 0, size = 5, sort="id", direction = Sort.Direction.ASC) Pageable pageable,
-                                 @RequestParam(value = "keyword") String keyword, Model model){
+    public String searchUserList(@PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
+                                 @RequestParam(value = "keyword") String keyword, Model model) {
         Page<AdminUserDto> usersByKeyword = adminService.findUsersByKeyword(keyword, pageable);
 
-        int startPage=0;
-        int endPage=0;
+        int startPage = 0;
+        int endPage = 0;
 
-        if(!usersByKeyword.isEmpty()) {
-            HashMap<String, Integer> pages = calculatePage(usersByKeyword.getPageable().getPageNumber(), usersByKeyword.getTotalPages());
+        if (!usersByKeyword.isEmpty()) {
+            HashMap<String, Integer> pages = Pagination.calculatePage(usersByKeyword.getPageable().getPageNumber(), usersByKeyword.getTotalPages());
             startPage = pages.get("startPage");
             endPage = pages.get("endPage");
         }
 
         model.addAttribute("dto", usersByKeyword);
-        model.addAttribute("startPage",startPage);
+        model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
         model.addAttribute("keyword", keyword);
 
@@ -66,14 +71,14 @@ public class AdminController {
     }
 
     @PutMapping("/userlist")
-    public ResponseEntity<?> updateUserRole(Long userId, String role){
+    public ResponseEntity<?> updateUserRole(Long userId, String role) {
         adminService.updateUserRole(userId, role);
 
         return ResponseEntity.ok("수정되었습니다.");
     }
 
     @DeleteMapping("/userlist")
-    public ResponseEntity<?> deleteUser(Long userId){
+    public ResponseEntity<?> deleteUser(Long userId) {
         adminService.deleteUser(userId);
 
         return ResponseEntity.ok("삭제되었습니다.");
@@ -81,37 +86,37 @@ public class AdminController {
 
     //수량 관리
     @GetMapping("/stocklist")
-    public String allStockList(@PageableDefault(page = 0, size = 5, sort="id", direction = Sort.Direction.ASC) Pageable pageable,
-                               Model model){
+    public String allStockList(@PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
+                               Model model) {
         Page<BookDto> allBook = adminService.findAllBook(pageable);
 
-        HashMap<String,Integer> pages=calculatePage(allBook.getPageable().getPageNumber(), allBook.getTotalPages());
-        int startPage=pages.get("startPage");
-        int endPage=pages.get("endPage");
+        HashMap<String, Integer> pages = Pagination.calculatePage(allBook.getPageable().getPageNumber(), allBook.getTotalPages());
+        int startPage = pages.get("startPage");
+        int endPage = pages.get("endPage");
 
         model.addAttribute("dto", allBook);
-        model.addAttribute("startPage",startPage);
+        model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
 
         return "admin/stocklist";
     }
 
     @GetMapping("/stocklist/search")
-    public String searchStockList(@PageableDefault(page = 0, size = 5, sort="id", direction = Sort.Direction.ASC) Pageable pageable,
-                                  @RequestParam(value = "keyword") String keyword, Model model){
+    public String searchStockList(@PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
+                                  @RequestParam(value = "keyword") String keyword, Model model) {
         Page<BookDto> booksByKeyword = adminService.findBooksByKeyword(keyword, pageable);
 
-        int startPage=0;
-        int endPage=0;
+        int startPage = 0;
+        int endPage = 0;
 
-        if(!booksByKeyword.isEmpty()) {
-            HashMap<String, Integer> pages = calculatePage(booksByKeyword.getPageable().getPageNumber(), booksByKeyword.getTotalPages());
+        if (!booksByKeyword.isEmpty()) {
+            HashMap<String, Integer> pages = Pagination.calculatePage(booksByKeyword.getPageable().getPageNumber(), booksByKeyword.getTotalPages());
             startPage = pages.get("startPage");
             endPage = pages.get("endPage");
         }
 
         model.addAttribute("dto", booksByKeyword);
-        model.addAttribute("startPage",startPage);
+        model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
         model.addAttribute("keyword", keyword);
 
@@ -119,53 +124,52 @@ public class AdminController {
     }
 
     @PutMapping("/stocklist")
-    public ResponseEntity<?> updateBookStock(Long bookId, Long stock){
+    public ResponseEntity<?> updateBookStock(Long bookId, Long stock) {
         adminService.updateBookStock(bookId, stock);
 
         return ResponseEntity.ok("수정되었습니다.");
     }
 
     @DeleteMapping("/stocklist")
-    public ResponseEntity<?> deleteBook(Long bookId){
+    public ResponseEntity<?> deleteBook(Long bookId) {
         adminService.deleteBook(bookId);
 
         return ResponseEntity.ok("삭제되었습니다");
     }
 
-
     //책 관리
     @GetMapping("/booklist")
-    public String allBooklist(@PageableDefault(page = 0, size = 5, sort="id", direction = Sort.Direction.ASC) Pageable pageable,
-                               Model model){
+    public String allBooklist(@PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
+                              Model model) {
         Page<BookDto> allBook = adminService.findAllBook(pageable);
 
-        HashMap<String,Integer> pages=calculatePage(allBook.getPageable().getPageNumber(), allBook.getTotalPages());
-        int startPage=pages.get("startPage");
-        int endPage=pages.get("endPage");
+        HashMap<String, Integer> pages = Pagination.calculatePage(allBook.getPageable().getPageNumber(), allBook.getTotalPages());
+        int startPage = pages.get("startPage");
+        int endPage = pages.get("endPage");
 
         model.addAttribute("dto", allBook);
-        model.addAttribute("startPage",startPage);
+        model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
 
         return "admin/booklist";
     }
 
     @GetMapping("/booklist/search")
-    public String searchBookList(@PageableDefault(page = 0, size = 5, sort="id", direction = Sort.Direction.ASC) Pageable pageable,
-                                 @RequestParam(value = "keyword") String keyword, Model model){
+    public String searchBookList(@PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
+                                 @RequestParam(value = "keyword") String keyword, Model model) {
         Page<BookDto> booksByKeyword = adminService.findBooksByKeyword(keyword, pageable);
 
-        int startPage=0;
-        int endPage=0;
+        int startPage = 0;
+        int endPage = 0;
 
-        if(!booksByKeyword.isEmpty()) {
-            HashMap<String, Integer> pages = calculatePage(booksByKeyword.getPageable().getPageNumber(), booksByKeyword.getTotalPages());
+        if (!booksByKeyword.isEmpty()) {
+            HashMap<String, Integer> pages = Pagination.calculatePage(booksByKeyword.getPageable().getPageNumber(), booksByKeyword.getTotalPages());
             startPage = pages.get("startPage");
             endPage = pages.get("endPage");
         }
 
         model.addAttribute("dto", booksByKeyword);
-        model.addAttribute("startPage",startPage);
+        model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
         model.addAttribute("keyword", keyword);
 
@@ -173,32 +177,69 @@ public class AdminController {
     }
 
     @GetMapping("/booklist/edit")
-    public String getBookEditForm(Long bookId, Model model){
+    public String getBookEditForm(Long bookId, Model model) {
 
         BookDto book = adminService.findBookById(bookId);
 
-        model.addAttribute("book",book);
+        model.addAttribute("book", book);
 
         return "admin/book-edit-form";
     }
 
     @PutMapping("/booklist/edit")
-    public ResponseEntity<?> updateBook(BookDto bookDto){
+    public ResponseEntity<?> updateBook(BookDto bookDto) {
 
         adminService.updateBook(bookDto);
 
         return ResponseEntity.ok("수정되었습니다.");
     }
 
-    private HashMap<String, Integer> calculatePage(int currentPage, int totalPage){
-        HashMap<String, Integer> result= new HashMap<>();
+    // 신간 등록
+    @GetMapping("/book_registration")
+    public String getBookRegistrationPage(Model model) {
 
-        int startPage=(currentPage/VISIBLE_PAGE)*VISIBLE_PAGE+1;
-        int endPage=Math.min(totalPage, (currentPage/VISIBLE_PAGE)*VISIBLE_PAGE+VISIBLE_PAGE);
+        List<BookDto> books = new ArrayList<>();
 
-        result.put("startPage", startPage);
-        result.put("endPage", endPage);
+        model.addAttribute("books", books);
+        model.addAttribute("totalPage", 0);
+        model.addAttribute("startPage", 0);
+        model.addAttribute("endPage", 0);
 
-        return result;
+        return "admin/book-registration";
+    }
+
+    @GetMapping("/book_registration/search")
+    public String searchNewBook(String keyword, @RequestParam(defaultValue = "1") int page, Model model) {
+
+        HashMap<String, ?> newBook = adminService.getNewBook(keyword, page);
+
+        List<BookDto> books = (List<BookDto>) newBook.get("books");
+        int totalPage = (int) newBook.get("totalPage");
+
+        int startPage = 0;
+        int endPage = 0;
+
+        if (!books.isEmpty()) {
+            HashMap<String, Integer> pages = Pagination.calculatePage(page - 1, totalPage);
+            startPage = pages.get("startPage");
+            endPage = pages.get("endPage");
+        }
+
+        model.addAttribute("books", books);
+        model.addAttribute("totalPage", totalPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("currentPage", page);
+
+        return "admin/book-registration";
+    }
+
+    @PostMapping("/book_registration")
+    public ResponseEntity<?> registrationNewBook(@RequestBody List<BookDto> books) {
+
+        adminService.addNewBook(books);
+
+        return ResponseEntity.ok("등록되었습니다.");
     }
 }
--- src/main/java/com/example/todaysbook/service/AdminServiceImpl.java
+++ src/main/java/com/example/todaysbook/service/AdminServiceImpl.java
@@ -4,14 +4,28 @@ import com.example.todaysbook.domain.dto.AdminUserDto;
 import com.example.todaysbook.domain.dto.BookDto;
 import com.example.todaysbook.domain.entity.Book;
 import com.example.todaysbook.domain.entity.User;
+import com.example.todaysbook.exception.aladinApi.NotValidBook;
 import com.example.todaysbook.repository.BookRepository;
 import com.example.todaysbook.repository.UserRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,6 +33,12 @@ import java.util.Optional;
 public class AdminServiceImpl implements AdminService {
     private final UserRepository userRepository;
     private final BookRepository bookRepository;
+
+    @Value("${aladin.ttbkey}")
+    private String aladinKey;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Override
     public Page<AdminUserDto> findAllUser(Pageable pageable) {
@@ -31,7 +51,7 @@ public class AdminServiceImpl implements AdminService {
         Page<User> userList = userRepository
                 .findByEmailContainingOrNickNameContaining(keyword, keyword, pageable);
 
-        if(userList.isEmpty()){
+        if (userList.isEmpty()) {
             return Page.empty();
         }
 
@@ -49,7 +69,7 @@ public class AdminServiceImpl implements AdminService {
     public Page<BookDto> findBooksByKeyword(String keyword, Pageable pageable) {
         Page<Book> bookList = bookRepository.findByAuthorContainingOrTitleContaining(keyword, keyword, pageable);
 
-        if(bookList.isEmpty()){
+        if (bookList.isEmpty()) {
             return Page.empty();
         }
 
@@ -118,7 +138,67 @@ public class AdminServiceImpl implements AdminService {
                 bookDto.getPublisher(), bookDto.getStock(), bookDto.getDescription());
     }
 
-    private AdminUserDto convertUserToDto(User user){
+    @Override
+    public HashMap<String, ?> getNewBook(String keyword, int page) {
+
+        String url = "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?Query=" + keyword +
+                "&ttbkey=" + aladinKey +
+                "&MaxResults=10&start=" + page +
+                "&SearchTarget=Book&Version=20131101&output=js" +
+                "&QueryType=Title&sort=Accuracy&Cover=Big";
+
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+        String jsonResponse = responseEntity.getBody();
+
+        List<BookDto> books = new ArrayList<>();
+        int totalResults = 0;
+
+        try {
+            JsonNode rootNode = objectMapper.readTree(jsonResponse);
+            JsonNode itemsNode = rootNode.get("item");
+
+            if (itemsNode != null && itemsNode.isArray()) {
+                for (JsonNode itemNode : itemsNode) {
+                    BookDto bookDto = convertJsonToBookDto(itemNode);
+
+                    books.add(bookDto);
+                }
+                totalResults = rootNode.get("totalResults").asInt();
+            }
+        } catch (Exception e) {
+            throw new NotValidBook();
+        }
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("books", books);
+        map.put("totalPage", (int) Math.ceil(totalResults / 10.0));
+
+        return map;
+    }
+
+    @Override
+    public void addNewBook(List<BookDto> bookList) {
+        for (BookDto book : bookList) {
+            Book newBook = Book.builder()
+                    .title(book.getTitle())
+                    .price(book.getPrice())
+                    .author(book.getAuthor())
+                    .description(book.getDescription())
+                    .publisher(book.getPublisher())
+                    .publishDate(book.getPublishDate())
+                    .stock(book.getStock())
+                    .isbn(book.getIsbn())
+                    .imagePath(book.getImage())
+                    .categoryId(book.getCategory())
+                    .build();
+
+            bookRepository.save(newBook);
+        }
+    }
+
+    private AdminUserDto convertUserToDto(User user) {
         return AdminUserDto.builder()
                 .id(user.getId())
                 .email(user.getEmail())
@@ -127,12 +207,93 @@ public class AdminServiceImpl implements AdminService {
                 .build();
     }
 
-    private BookDto convertBookToDto(Book book){
+    private BookDto convertBookToDto(Book book) {
         return BookDto.builder()
                 .id(book.getId())
                 .title(book.getTitle())
                 .price(book.getPrice())
                 .stock(book.getStock())
                 .build();
+    }
+
+    private BookDto convertJsonToBookDto(JsonNode itemNode) {
+        BookDto book = BookDto.builder()
+                .title(itemNode.get("title").asText())
+                .author(itemNode.get("author").asText())
+                .price(itemNode.get("priceStandard").asLong())
+                .image(itemNode.get("cover").asText())
+                .publisher(itemNode.get("publisher").asText())
+                .publishDate(dateFomatter(itemNode.get("pubDate").asText()))
+                .isbn(itemNode.get("isbn13").asText())
+                .category(convertCategoryToCategoryId(itemNode.get("categoryName").asText()))
+                .stock(-1L)
+                .description(itemNode.get("description").asText())
+                .build();
+
+        return book;
+    }
+
+    private LocalDateTime dateFomatter(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate localDate = LocalDate.parse(date, formatter);
+        return localDate.atStartOfDay(); //날짜 + 00:00:00.00000000을 의미
+    }
+
+    private String convertCategoryToCategoryId(String category) {
+        //카테고리가 빈값일 때
+        if (category.isEmpty()) {
+            return "123";
+        }
+
+        String[] splitCategory = category.split(">");
+
+        String categoryName = splitCategory[1];
+        if (categoryName.equals("자기계발")) {
+            return "101";
+        } else if (categoryName.equals("어린이")) {
+            return "102";
+        } else if (categoryName.equals("외국어")) {
+            return "103";
+        } else if (categoryName.equals("인문학")) {
+            return "104";
+        } else if (categoryName.equals("유아")) {
+            return "105";
+        } else if (categoryName.equals("만화")) {
+            return "106";
+        } else if (categoryName.equals("경제경영")) {
+            return "107";
+        } else if (categoryName.equals("소설/시/희곡")) {
+            return "108";
+        } else if (categoryName.equals("에세이")) {
+            return "109";
+        } else if (categoryName.equals("수험서/자격증")) {
+            return "110";
+        } else if (categoryName.equals("예술/대중문화")) {
+            return "111";
+        } else if (categoryName.equals("컴퓨터/모바일")) {
+            return "112";
+        } else if (categoryName.equals("청소년")) {
+            return "113";
+        } else if (categoryName.equals("과학")) {
+            return "114";
+        } else if (categoryName.equals("사회과학")) {
+            return "115";
+        } else if (categoryName.equals("좋은부모")) {
+            return "116";
+        } else if (categoryName.equals("건강/취미")) {
+            return "117";
+        } else if (categoryName.equals("역사")) {
+            return "118";
+        } else if (categoryName.equals("종교/역학")) {
+            return "119";
+        } else if (categoryName.equals("요리/살림")) {
+            return "120";
+        } else if (categoryName.equals("여행")) {
+            return "121";
+        } else if (categoryName.equals("대학교재/전문서적")) {
+            return "122";
+        } else {
+            return "123";
+        }
     }
 }
