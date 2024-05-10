61727a47c31efce6367e96e0dfca0de7d53a14b9
fernandokkang <spineattack@gmail.com>
2024-05-10 09:53:22 +0900

    feat: 스프링 시큐리티 권한 부여

--- src/main/java/com/example/todaysbook/controller/UserController.java
+++ src/main/java/com/example/todaysbook/controller/UserController.java
@@ -3,42 +3,20 @@ package com.example.todaysbook.controller;
 import com.example.todaysbook.domain.dto.*;
 import com.example.todaysbook.domain.entity.User;
 import com.example.todaysbook.service.UserService;
-import com.example.todaysbook.service.CustomUserDetailsService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-    private final CustomUserDetailsService customUserDetailsService;
-    private final AuthenticationManager authenticationManager;
     private final PasswordEncoder encoder;
 
-    public UserController (UserService userService, CustomUserDetailsService customUserDetailsService,AuthenticationManager authenticationManager, PasswordEncoder encoder) {
-        this.userService = userService;
-        this.customUserDetailsService = customUserDetailsService;
-        this.authenticationManager = authenticationManager;
-        this.encoder = encoder;
-    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody UserRequestDto request) {
@@ -56,7 +34,7 @@ public class UserController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/login")
+    /*@PostMapping("/login")
     public ResponseEntity<?> login(HttpServletRequest request, HttpServletResponse response,
                                    @RequestBody LoginRequestDto loginRequestDto){
         UserDetails userDetails = customUserDetailsService.loadUserByUsername(loginRequestDto.getEmail());
@@ -108,7 +86,7 @@ public class UserController {
 
         System.out.println("logout success");
         return ResponseEntity.ok().build();
-    }
+    }*/
 
     @PutMapping ("/update/nickname")
     public ResponseEntity<?> updateUserNickname(@RequestBody UserRequestDto request, @AuthenticationPrincipal CustomUserDetails userDetails) {
