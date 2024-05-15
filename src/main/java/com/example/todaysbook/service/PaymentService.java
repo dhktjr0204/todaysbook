c1112bb20c2c1df305f85df4f47592c3d7316647
DESKTOP-B8HHBMV\Administrator <udidcs@naver.com>
2024-05-15 13:23:16 +0900

    Feature: 구매하기 구현

--- src/main/java/com/example/todaysbook/service/PaymentService.java
+++ src/main/java/com/example/todaysbook/service/PaymentService.java
@@ -7,6 +7,5 @@ import java.util.List;
 
 public interface PaymentService {
     void createOrder(long userId, List<PaymentBookInfoDto> bookDtoList, PaymentAddressAndMileageInfo addressAndMileageInfo);
-
     void subtractStock(List<PaymentBookInfoDto> books);
 }
